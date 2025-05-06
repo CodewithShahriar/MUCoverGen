62af5b38c80f30fab5cea747a6639c8ed5c0e8d9
Abid Shahriar <121728537+CodewithShahriar@users.noreply.github.com>
2025-05-06 20:05:28 +0600

    export faculty list

--- src/data/facultyData.ts
+++ src/data/facultyData.ts
@@ -7,5 +7,39 @@ export interface Faculty {
 }
 
 export const facultyList: Faculty[] = [
-  
+  { id: "2", name: "Professor Dr. Md. Nazrul Haque Chowdhury", position: "Dean, School of Science & Technology", department: "Computer Science & Engineering" },
+  { id: "4", name: "Md. Mahfuzul Hasan", position: "Associate Professor", department: "Computer Science & Engineering" },
+  { id: "5", name: "Safwan Uddin Ahmed", position: "Assistant Professor", department: "Computer Science & Engineering" },
+  { id: "6", name: "Md. Mustafiz Shahriar Rabe", position: "Assistant Professor", department: "Computer Science & Engineering" },
+  { id: "7", name: "Archi Arani Basak", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "8", name: "Khudeja Khanom Anwara", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "9", name: "Md. Shamihul Islam Khan Limon", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "10", name: "Nabila Jannat Rifa", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "11", name: "Nasif Istiak Remon", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "12", name: "Rishad Amin Pulok", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "13", name: "Iftekhar Hossain", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "14", name: "Salma Akther", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "15", name: "Abu Jafar Md Jakaria", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "16", name: "Mayumi Das Purkayastha Purba", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "17", name: "Bushra Azmat Hussain", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "18", name: "Ruma Das", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "19", name: "Md. Fahmidaur Rahman Sakib", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "20", name: "Sharbanit Chowdhury", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "21", name: "Muhammad Muzammil", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "22", name: "Moshiur Ahmed", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "23", name: "Samia Rahman Rima", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "24", name: "Rahul Amin", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "25", name: "Nazrul Islam", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "26", name: "Tajbin Jahan", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "27", name: "Nowshin Sharmin", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "28", name: "Dewan Ahmed Muhtasim", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "29", name: "Aisha Hayder Chowdhury", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "30", name: "Piabon Talukder", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "31", name: "Abdullah Al Masud", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "32", name: "Golam Mostofa Naeem", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "33", name: "Abdul Wadud Shakib", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "34", name: "Raisa Fairooz", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "35", name: "Khulud Binte Harun", position: "Lecturer", department: "Computer Science & Engineering" },
+  { id: "36", name: "Farhana Akter", position: "Teaching Assistant", department: "Computer Science & Engineering" }
 ];
+
