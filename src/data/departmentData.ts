ac3245205ebc6717ae09a725e1e260121fd87db5
Abid Shahriar <121728537+CodewithShahriar@users.noreply.github.com>
2025-05-06 20:07:51 +0600

    fix: depterment data bba

--- src/data/departmentData.ts
+++ src/data/departmentData.ts
@@ -9,7 +9,7 @@ export const departmentList: Department[] = [
   { id: "1", name: "Computer Science & Engineering", shortName: "CSE" },
   { id: "2", name: " Department of Software Engineering", shortName: "SWE" },
   { id: "3", name: "Department of Electrical & Electronic Engineering", shortName: "EEE" },
-  { id: "4", name: "English Literature", shortName: "English" },
+  { id: "4", name: "Department of Business Administration", shortName: "BBA" },
   { id: "5", name: "Physics", shortName: "PHY" },
   { id: "6", name: "Chemistry", shortName: "CHEM" },
   { id: "7", name: "Mathematics", shortName: "MATH" },
